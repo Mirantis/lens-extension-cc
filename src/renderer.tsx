af4972d25e3da956f1537144a302bb5b42c9b863
Stefan Cameron <stefan@stefcameron.com>
2020-11-12 12:46:02 -0600

    Fix status bar item styling after beta.1

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { LensRendererExtension } from '@k8slens/extensions';
 import { AddClusterIcon, AddClusterPage } from './page';
 import * as strings from './strings';
 import { addRoute } from './routes';
+
+const Item = styled.div(function () {
+  return {
+    color: 'var(--textColorAccent)',
+    cursor: 'pointer',
+    fontSize: 'var(--font-size-small)',
+  };
+});
 
 export default class ExtensionRenderer extends LensRendererExtension {
   globalPages = [
@@ -17,13 +26,13 @@ export default class ExtensionRenderer extends LensRendererExtension {
   statusBarItems = [
     {
       item: (
-        <div
-          className="flex align-center gaps hover-highlight"
+        <Item
+          className="flex align-center gaps"
           onClick={() => this.navigate(addRoute)}
         >
           <AddClusterIcon />
           <span>{strings.extension.statusBar['label']()}</span>
-        </div>
+        </Item>
       ),
     },
   ];
