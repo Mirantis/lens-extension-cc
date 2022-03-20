21acbaa455bde35fa6741c93cb71aedebe8dbcc4
anos <anos@mirantis.com>
2022-03-20 19:45:37 +0200

    changed extension button color

--- src/renderer/topBar.tsx
+++ src/renderer/topBar.tsx
@@ -5,15 +5,10 @@ import { GlobalPageIcon } from './components/GlobalPage/GlobalPage';
 import { layout } from './components/styles';
 import * as strings from '../strings';
 
-const statusItemColor = '#8F9296';
-
 const TopBarExtension = styled.div`
-  position: absolute;
-  top: ${layout.grid * 1.25}px;
-  right: ${layout.grid * 15}px;
   display: flex;
   align-items: center;
-  background-color: var(--mainBackground);
+  background-color: var(--layoutTabsBackground);
   padding: ${layout.pad * 0.25}px ${layout.pad * 0.5}px;
   border-radius: 4px;
   cursor: pointer;
@@ -22,7 +17,7 @@ const TopBarExtension = styled.div`
 const TopBarExtensionTitle = styled.p`
   font-size: calc(var(--font-size) * 0.85);
   line-height: 1.17;
-  color: #c4c4c4;
+  color: var(--textColorPrimary);
   max-width: ${layout.grid * 22.5}px;
   margin-left: ${layout.grid * 1.25}px;
 `;
@@ -36,7 +31,7 @@ export function registerTopBarItems(extension) {
             <TopBarExtension
               onClick={() => extension.navigate(ROUTE_GLOBAL_PAGE)}
             >
-              <GlobalPageIcon size={24} fill={statusItemColor} />
+              <GlobalPageIcon size={24} fill="var(--textColorPrimary)" />
               <TopBarExtensionTitle>
                 {strings.extension.statusBar['label']()}
               </TopBarExtensionTitle>
