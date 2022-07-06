923164d9dc08ddfa3b97493c51080a45ec25bd53
Anton Nos <anos@anos.local>
2022-07-06 23:52:36 +0300

    fixed types error in ContainerCloudIcon.spec file to prevent crashing lint tests

--- src/renderer/components/__tests__/ContainerCloudIcon.spec.js
+++ src/renderer/components/__tests__/ContainerCloudIcon.spec.js
@@ -7,24 +7,24 @@ describe('/renderer/components/ContainerCloudIcon', () => {
 
     const icon = screen.getBySelector('svg');
 
-    const defaultIconSize = '16';
+    const defaultIconSize = 16;
     const defaultIconColor = 'currentColor';
 
-    expect(icon).toHaveAttribute('width', defaultIconSize);
-    expect(icon).toHaveAttribute('height', defaultIconSize);
+    expect(icon).toHaveAttribute('width', `${defaultIconSize}`);
+    expect(icon).toHaveAttribute('height', `${defaultIconSize}`);
     expect(icon).toHaveStyle(`fill: ${defaultIconColor}`);
   });
 
   it('renders an icon with custom size and color', () => {
-    const customIconSize = '24';
+    const customIconSize = 24;
     const customIconColor = '#000000';
 
     render(<ContainerCloudIcon size={customIconSize} fill={customIconColor} />);
 
     const icon = screen.getBySelector('svg');
 
-    expect(icon).toHaveAttribute('width', customIconSize);
-    expect(icon).toHaveAttribute('height', customIconSize);
+    expect(icon).toHaveAttribute('width', `${customIconSize}`);
+    expect(icon).toHaveAttribute('height', `${customIconSize}`);
     expect(icon).toHaveStyle(`fill: ${customIconColor}`);
   });
 });
